90404abd720feb262d0225b71ceb84d8ed3154d6
cancer <comparan77@gmail.com>
2017-06-26 23:41:21 -0500

    validar registro

--- js/controller/personalController.js
+++ js/controller/personalController.js
@@ -15,7 +15,10 @@ var PerController = function() {
                 var beanPersona = new BeanPersonalQr(result.text, oUsuario.Id_bodega);
                 CatalogosModel.PersonalRegistro(beanPersona, function(data) {
                     x$('#div_foto').removeClass('hidden');
+                    x$('#p_mensaje').html(data.Mensaje);
                     x$('#img_foto').attr('src', urlHandler + 'rpt/personal/' + data.Idf + '/Foto.jpg');
+                    if(data.PPerReg==null)
+                        x$('#p_mensaje').addClass('error');
                     //scanear();
                 });
             },
@@ -28,6 +31,8 @@ var PerController = function() {
     function btn_registrar_click() {
         try {
             x$('#btn_registrar').on('click', function() {
+                x$('#p_mensaje').html('');
+                x$('#p_mensaje').removeClass('error');
                 x$('#div_foto').addClass('hidden');
                 scanear();
             });        
